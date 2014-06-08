78ef26149b7fab607c8db0c70b0f280e8b59fb79
Mark Soper <masoper@gmail.com>
2014-06-08 17:43:52 -0400

    problems with require:

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :description "overlapping square tile art/game"
   :source-paths ["src-clj"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2197"
-                  :exclusions [org.apache.ant/ant]]
-                 [org.clojure/data.json "0.2.4"]]
+                 [org.clojure/clojurescript "0.0-2197"]
+                 [cheshire "5.3.1"]]
   :plugins [[lein-cljsbuild "1.0.3"]]
   :cljsbuild {
     :builds [{:source-paths ["src-cljs"]
--- src-cljs/tile.cljs
+++ src-cljs/tile.cljs
@@ -1,13 +1,14 @@
 
-(ns tile)
-
-(require '[clojure.data.json :as json])
+(ns tile
+  (:require [cheshire.core :as json]))
 
 (def area {:x 0 :y 0 :h 1000 :w 1000})
 (def tile {
   :x (/ (- (get area :w) (get area :x)) 2)
   :y (/ (- (get area :h) (get area :y)) 2)
 })
-(defn tileToSVG [tile]
+(json/generate-string tile)
+(defn tileToJSON [tile]
+  ;json/write-str tile
   (str "<rect x=\"" (get tile :x) "\"")
 )
